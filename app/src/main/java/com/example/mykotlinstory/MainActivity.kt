3657affdc3e367766cf6f0d9883d78cff8fac428
Abdulmohsenmtq <3malmutlaq@gmail.com>
2021-12-12 23:36:35 +0300

    Create Navigatoin view added menu folder and icons create menu_drawer.xml added strings in strings.xml

--- app/src/main/java/com/example/mykotlinstory/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinstory/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.mykotlinstory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
     private var textViewEmail:TextView? = null
+    private var drawerLayout:DrawerLayout? = null
+    private var toolbarView:Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +23,34 @@ class MainActivity : AppCompatActivity() {
 
         connectViews()
         textViewEmail?.text = email
+        setSupportActionBar(toolbarView)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setupDrawer()
 
     }
+    private fun setupDrawer(){
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                drawerLayout?.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> true
+        }
+    }
+
     private fun connectViews(){
         textViewEmail = findViewById(R.id.tvEmail)
+        drawerLayout = findViewById(R.id.drawer)
+        toolbarView = findViewById(R.id.toolbar)
     }
 
 }
